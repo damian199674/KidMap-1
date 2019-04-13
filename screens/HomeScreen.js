1f00ac1f2023efa0b3c8afd8ca24788d4f4b11b6
jaksa <jkrzemien28@gmail.com>
2019-04-13 18:11:25 +0200

    init commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,56 +6,46 @@ import {
     Text,
     View,
 } from 'react-native';
+import QRCode from 'react-native-qrcode';
 
 export default class HomeScreen extends React.Component {
-    static navigationOptions = {
-        header: null,
-    };
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-                    <View style={styles.welcomeContainer}>
-                        <Image
-                            source={require('../assets/images/family.png')}
-                            style={styles.welcomeImage}
-                        />
-                    </View>
-                    <Text style={styles.getStartedText}>Welcome to Kid Map!</Text>
-
-                    <Text style={styles.getStartedText}>
-                        Create an account or log in.</Text>
-                </ScrollView>
-            </View>
-        );
-    }
+state = {
+    text: 'http://facebook.github.io/react-native/',
+  };
+ 
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => this.setState({text: text})}
+          value={this.state.text}
+        />
+        <QRCode
+          value={this.state.text}
+          size={200}
+          bgColor='purple'
+          fgColor='white'/>
+      </View>
+    );
+  };
 }
-
+ 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-    },
-    contentContainer: {
-        paddingTop: 30,
-    },
-    welcomeContainer: {
+        backgroundColor: 'white',
         alignItems: 'center',
-        marginTop: 10,
-        marginBottom: 20,
+        justifyContent: 'center'
     },
-    welcomeImage: {
-        width: 100,
-        height: 80,
-        resizeMode: 'contain',
-        marginTop: 3,
-        marginLeft: -10,
-    },
-    getStartedText: {
-        fontSize: 20,
-        color: 'rgba(96,100,109, 1)',
-        lineHeight: 24,
-        textAlign: 'center',
-    },
+ 
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        margin: 10,
+        borderRadius: 5,
+        padding: 5,
+    }
 });
+ 
