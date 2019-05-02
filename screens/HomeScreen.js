e3fd8a5ebeb03e1fbd4d15af907f4a98b4b097f0
Damian Górski <damiangorski199674@gmail.com>
2019-05-02 11:24:25 +0200

    update functionality of login

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,208 +1,155 @@
-import React from 'react';
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base';
-import * as firebase from 'firebase';
+import React, { Component } from 'react';
+import { View, Text, Image} from 'react-native';
+import firebase from 'firebase';
+import {Input, Form, Item, Button, Label} from 'native-base';
 
+export default class HomeScreen extends Component {
+    state = { logged: null };
 
-//KEYS
-const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-  };
-  
-firebase.initializeApp(firebaseConfig);
-
-class HomeScreen extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = ({
-            email: '',
-            password: ''
+    //FIREBASE 
+    componentWillMount(){
+        const Firebaseconfig = {
+            apiKey: "",
+            authDomain: "",
+            databaseURL: "",
+            projectId: "",
+            storageBucket: "",
+            };
+        
+        firebase.initializeApp(Firebaseconfig);
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ logged: true })
+            } else {
+                this.setState({ logged: false })
+            }
         });
     }
     static navigationOptions = {
-        header: null,
+         title: 'Home',
     };
 
-
-    login = (email, password) => {
-        try {
-            firebase.auth().signInWithEmailAndPassword(email, password)
-        }
-        catch (error) {
-            alert("Incorrect e-mail or password")
+    renderComponent() {
+        if (this.state.logged) {
+            return (
+                <Button style={{ padding: 5 }}
+                block
+                rounded
+                info
+                onPress={() => firebase.auth().signOut()}
+            >
+                <Text> Sign out</Text>
+            </Button>
+            );
+        } else {
+            return (
+                <LoginForm />
+            );
         }
     }
-
-
-
     render() {
-        const{ navigate } = this.props.navigation;
         return (
-            <Container style={styles.container}>
-                <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-                    <View style={styles.welcomeContainer}>
-                        <Image
-                            source={require('../assets/images/family.png')}
-                            style={styles.welcomeImage}
-                        />
-                    </View>
-                    <Text style={styles.getStartedText}>Welcome to Kid Map!</Text>
-
-                    <Text style={styles.getStartedText}>
-                        Create an account or log in.</Text>
-                
-                    <Form style={styles.form}>
-                        <Item inlineLabel>
-                            <Label>Email:</Label>
-                                <Input
-                                    onChangeText={(email) => this.setState({ email })}
-                                />
-                        </Item>
-                        <Item inlineLabel>
-                            <Label>Password:</Label>
-                                <Input
-                                    secureTextEntry={true}
-                                    onChangeText={(password) => this.setState({ password })}
-                                />
-                        </Item>
-                        <Button style={{ padding: 5 }}
-                            block
-                            rounded
-                            success
-                            onPress={() => this.login(this.state.email, this.state.password)}
-                        >
-                            <Text> Login</Text>
-                        </Button>
-                        <Button style={{ padding: 5 }}
-                            block
-                            rounded
-                            info
-                            onPress={() => navigate('Register')}
-                        >
-                            <Text> Join free!</Text>
-                        </Button>
-                        <Button style={{ padding: 5 }}
-                            block
-                            rounded
-                            light
-                            onPress={() => alert('Facebook')}
-                        >
-                            <Text> Facebook</Text>
-                        </Button>
-                    </Form>
-                </ScrollView>
-            </Container>
+            <View>
+                {this.renderComponent()}
+            </View>
         );
     }
 }
 
-class RegisterScreen extends React.Component {
+class LoginForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { email: '', password: '', error: '' };
+    }
     static navigationOptions = {
-        header: null,
+        title: 'Add user',
     };
+  
+    onButtonPress() {
+      this.setState({ error: ''})
+      const { email, password } = this.state;
+      firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(this.login.bind(this))
+            .catch((error) => {
+                alert(error.message)      
+            });
+          }
     
-    signUp = (email, password) => {
-        try {
-            if (this.state.password.length < 6) {
-                alert("Please enter at least 6 characters")
-                return;
-            }    
-            firebase.auth().createUserWithEmailAndPassword(email, password)
-        }
-        catch (error) {
-            alert('Something went wrong')
-        }
+    login() {
+      this.setState({
+        email: '', password: '', error: ''
+      })
     }
-
+  
     render() {
-        const{ navigate } = this.props.navigation;
-        return (
-            <Container style={styles.container}>
-                <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-                    <Form style={styles.form}>
-                        <Item inlineLabel>
-                            <Label>Email:</Label>
-                                <Input
-                                    onChangeText={(email) => this.setState({ email })}
-                                />
-                        </Item>
-                        <Item inlineLabel>
-                            <Label>Password:</Label>
-                                <Input
-                                    secureTextEntry={true}
-                                    onChangeText={(password) => this.setState({ password })}
-                                />
-                        </Item>
-                        <Button style={{ padding: 5 }}
-                            block
-                            rounded
-                            info
-                            onPress={() => this.signUp(this.state.email, this.state.password)}
-                        >
-                            <Text> Sign Up</Text>
-                        </Button>
-                    </Form>
-                </ScrollView>
-            </Container>
-        );
+      return (
+          <View style={styles.container}>
+            <View style={styles.welcomeContainer}>
+            <Image
+                source={require('../assets/images/family.png')}
+                style={styles.welcomeImage}
+            />
+            </View>
+          <Text style={styles.getStartedText}>Welcome to Kid Map!</Text>
+  
+          <Text style={styles.getStartedText}>
+            Create an account or log in.</Text>
+      
+        <Form style={styles.form}>
+          <Item inlineLabel>
+            <Label>Email:</Label>
+              <Input
+                  value={this.state.email}
+                  autoCapitalize="none"
+                  onChangeText={(email) => this.setState({ email })}
+              />
+          </Item>
+          <Item inlineLabel>
+              <Label>Password:</Label>
+                  <Input
+                      secureTextEntry={true}
+                      value={this.state.password}
+                      autoCapitalize="none"
+                      onChangeText={(password) => this.setState({ password })}
+  
+                  />
+          </Item>
+          <Button style={{ padding: 5 }}
+          block
+          rounded
+          success
+          onPress={this.onButtonPress.bind(this)}
+          >
+            <Text> Login</Text>
+          </Button>
+        </Form>
+        </View>
+      );
     }
-}
-
-
-class PairScreen extends React.Component {
-    static navigationOptions = {
-        header: null,
-    };
-
-    render() {
-        return (
-            <Text>ADD YOUR KID</Text>
-        );
-    }
-}
-
-
-const AppNavigator = createStackNavigator({
-  Home: { screen: HomeScreen },
-  Register : { screen: RegisterScreen },
-  Pair : { screen: PairScreen },
-}, { initialRouteName: 'Home'});
-
-export default createAppContainer(AppNavigator);
-    
-const styles = StyleSheet.create({
+  }
+  
+  const styles = {
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    contentContainer: {
-        paddingTop: 30,
+      backgroundColor: '#fff',
     },
     welcomeContainer: {
-        alignItems: 'center',
-        marginTop: 10,
-        marginBottom: 20,
+      alignItems: 'center',
+      marginTop: 10,
+      marginBottom: 20,
     },
     welcomeImage: {
-        width: 80,
-        height: 60,
-        resizeMode: 'contain',
-        marginTop: 3,
-        marginLeft: -10,
+      width: 80,
+      height: 60,
+      resizeMode: 'contain',
+      margin: 'auto'
     },
     getStartedText: {
-        fontSize: 20,
-        color: 'rgba(96,100,109, 1)',
-        lineHeight: 24,
-        textAlign: 'center',
+      fontSize: 20,
+      color: 'rgba(96,100,109, 1)',
+      lineHeight: 24,
+      textAlign: 'center',
     },
     form: {
-        marginTop: 10,
+      marginTop: 10,
     },
-});
+  };
